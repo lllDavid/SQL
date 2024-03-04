a8128d1f745042a44fffb327e609bf1336520968
lllDavid <imperfectrising5527@gmail.com>
2024-03-04 12:19:22 +0100

    sync

---
+++ Skript/cd_database_data_temp.sql
@@ -0,0 +1,48 @@
+-- ###################################################
+-- CD Database Temporary Data
+-- temporary Table for demonstration purposes
+-- contains Smashing Pumpkins Song Titles for "Siamese Dream" and
+-- "Mellon Collie And The Infinite Sadness
+-- by Matthias Obermaier, 2021, using Data found on Wikipedia
+-- ###################################################
+
+CREATE TABLE sp_temp_songs (AlbumID INT, Track INT, Song VARCHAR(50));
+INSERT INTO sp_temp_songs VALUES
+    (1, 4, 'Hummer'),
+    (1, 5, 'Rocket'),
+    (1, 6, 'Disarm'),
+    (1, 7, 'Soma'),
+    (1, 8, 'Geek U.S.A.'),
+    (1, 9, 'Mayonaise'),
+    (1, 10, 'Spaceboy'),
+    (1, 11, 'Silverfuck'),
+    (1, 12, 'Sweet Sweet'),
+    (1, 13, 'Luna');
+
+INSERT INTO sp_temp_songs
+    VALUES (2, 4, 'Zero'),
+    (2, 5, 'Here Is No Why'),
+    (2, 6, 'Bullet With Butterfly Wings'),
+    (2, 7, 'To Forgive'),
+    (2, 8, 'Fuck You (An Ode To No One)'),
+    (2, 9, 'Love'),
+    (2, 10, 'Cupid De Locke'),
+    (2, 11, 'Galapogos'),
+    (2, 12, 'Muzzle'),
+    (2, 13, 'Porcelina Of The Vast Oceans'),
+    (2, 14, 'Take Me Down'),
+    (2, 15, 'Where Boys Fear To Tread'),
+    (2, 16, 'Bodies'),
+    (2, 17, 'Thirty-Three'),
+    (2, 18, 'In The Arms Of Sleep'),
+    (2, 19, '1979'),
+    (2, 20, 'Tales Of A Scorched Earth'),
+    (2, 21, 'Thru the Eyes of Ruby'),
+    (2, 22, 'Stumbleine'),
+    (2, 23, 'X.Y.U'),
+    (2, 24, 'We Only Come Out At Night'),
+    (2, 25, 'Beautiful'),
+    (2, 26, 'Lily (My One And Only)'),
+    (2, 27, 'By Starlight'),
+    (2, 28, 'Farewell And Goodnight');
+
