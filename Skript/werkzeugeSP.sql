3221dfd4cd7dbc2c8052c810c04bf67dd86da175
lllDavid <imperfectrising5527@gmail.com>
2024-07-07 11:46:16 +0200

    sync

--- Skript/werkzeugeSP.sql
+++ Skript/werkzeugeSP.sql
@@ -7,7 +7,7 @@ BEGIN
 
     -- Überprüfen, ob das Werkzeug existiert
     IF ((SELECT COUNT(*) FROM werkzeuge WHERE werkzeuge.id = id) = 0) THEN
-        SELECT 'Keine matchenden Ergebnisse';
+        SELECT 'Keine matchenden Ergebnisse' AS Nachricht;
     ELSE
         -- Ausgabe des Werkzeugs
         SELECT * FROM werkzeuge WHERE werkzeuge.id = id;
@@ -15,15 +15,11 @@ BEGIN
         -- Lagerbestand des Werkzeugs abrufen
         SELECT Lager INTO lager FROM werkzeuge WHERE werkzeuge.id = id;
 
-        -- Überprüfen, ob die Bestellmenge auf Lager ist
-        SELECT Lager,
-            IF(bestellmenge > Lager, 'So viel ist nicht auf Lager', 'Ok') AS Status
-        FROM werkzeuge
-        WHERE werkzeuge.id = id;
-
-        -- Überprüfen, ob die Bestellmenge gleich dem Lagerbestand ist
-        IF bestellmenge = lager THEN
-            SELECT 'Bestellung Ok!';
+        -- Überprüfen, ob die Bestellmenge gleich oder kleiner als der Lagerbestand ist
+        IF bestellmenge <= lager THEN
+            SELECT 'Bestellung Ok!' AS Nachricht;
+        ELSE
+            SELECT 'So viel ist nicht auf Lager' AS Nachricht;
         END IF;
     END IF;
 END //
